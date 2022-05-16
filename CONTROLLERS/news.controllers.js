847ca303dabeab58845647265a6acd90443d644d
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-05-16 14:56:32 +0100

    this commit tightens the code and tests for #3 GET/api/topics

--- CONTROLLERS/news.controllers.js
+++ CONTROLLERS/news.controllers.js
@@ -1,8 +1,7 @@
-const models = require('../MODELS/news.models');
-const {fetchTopics} = models;
+const {fetchTopics} = require('../MODELS/news.models');
 
 exports.getTopics = (req, res, next) => {
   return fetchTopics().then((topics) => {
-    res.status(200).send(topics);
+    res.status(200).send({topics});
   }).catch(next);
 };
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,11 +13,9 @@ beforeEach(() => {
 });
 
 describe('GET /not-a-route', () => {
-  test(`Status 404: should respond with a json object containing a 'message' key, constaining 'Not Found'`, () => {
-    request(app).get('/jibberish').expect(404).then((res)=>{
-      const {body} = res;
-      expect(body).toHaveProperty('message');
-      expect(body.message).toEqual('Not Found');
+  test(`Status 404: should respond with a json object containing a 'message' key, containing 'Not Found'`, () => {
+    request(app).get('/jibberish').expect(404).then(({body})=>{
+      expect(body).toHaveProperty('message', 'Not Found');
     });
   });
 });
@@ -25,11 +23,13 @@ describe('GET /not-a-route', () => {
 describe('GET /api/topics', () => {
   test(`should respond with an array of topic objects, each of which should have the following properties: 'slug', 'description'`, () => {
     return request(app).get('/api/topics').expect(200).then(({body}) => {
-      expect(body).toBeInstanceOf(Array);
-      body.forEach((topic)=> {
-        expect(topic).toHaveProperty('slug');
-        expect(topic).toHaveProperty('description');
-      });
+      expect(body.topics).toBeInstanceOf(Array);
+      if (body.topics.length > 0) {
+        body.topics.forEach((topic)=> {
+          expect(topic).toHaveProperty('slug');
+          expect(topic).toHaveProperty('description');
+        });
+      }
     });
   });
 });
