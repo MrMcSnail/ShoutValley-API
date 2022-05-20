148a1b38323f89234215e2a210be1ac94170788f
Kyle McPhail <99103416+MrMcSnail@users.noreply.github.com>
2022-05-20 22:37:13 +0100

    heroku setup as shoutvalley

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const app = express();
-const { getCommentsByArticleID, postCommentToArticle, removeCommentsByCommentID} = require('./CONTROLLERS/comments.controllers');
+const {getCommentsByArticleID, postCommentToArticle, removeCommentsByCommentID} = require('./CONTROLLERS/comments.controllers');
 const {getTopics} = require('./CONTROLLERS/topics.controllers');
 const {getArticleByID, patchVotesByArticleID, getAllArticles} = require('./CONTROLLERS/articles.controllers');
 const {getAllUsers} = require('./CONTROLLERS/users.controllers');
 const {handleCustomErrors, handlePsqlErrors, handleServerErrors,
 } = require('./CONTROLLERS/errors.controllers');
 const endpointMap = require('./endpoints.json');
+
 app.use(express.json());
 
 app.get('/api', (req, res, next)=> {
   res.status(200).send({endpointMap});
 });
-
 app.get('/api/topics', getTopics);
 app.get('/api/articles', getAllArticles);
 app.get('/api/articles/:article_id', getArticleByID);
@@ -29,7 +29,6 @@ app.post('/api/articles/:article_id/comments', postCommentToArticle);
 app.all('/*', (req, res) => {
   res.status(404).send({message: 'Not Found'});
 });
-
 app.use(handleCustomErrors);
 app.use(handlePsqlErrors);
 app.use(handleServerErrors);
